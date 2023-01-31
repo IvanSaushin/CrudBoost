c76ae68ffb8e3ef36049bb19fff00be332c2672d
mishe <mishelsim2@gmail.com>
2023-01-31 12:41:19 +0400

    MT-12

--- src/main/java/com/springboot/controller/MainRestController.java
+++ src/main/java/com/springboot/controller/MainRestController.java
@@ -5,15 +5,9 @@ import com.springboot.service.RoleService;
 import com.springboot.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLOutput;
 import java.util.List;
 
 @RestController
@@ -36,6 +30,11 @@ public class MainRestController {
         return userService.getUserById(id);
     }
 
+    @GetMapping("/search")
+    public List<User>search(@RequestParam ("name") String name) {
+        System.out.println("111: " + name);
+        return userService.getAllUsers();
+    }
 
     @PostMapping("/create")
     public ResponseEntity<User> create(@RequestBody User user) {
