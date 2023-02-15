09c8fd3bae94313af3c9258575de6b31c9749e5c
mishe <mishelsim2@gmail.com>
2023-02-15 12:08:36 +0400

    MT-3 update

--- src/main/java/com/springboot/service/impl/AdressServiceImpl.java
+++ src/main/java/com/springboot/service/impl/AdressServiceImpl.java
@@ -39,7 +39,8 @@ public class AdressServiceImpl implements AdressService {
 
     @Override
     public void update(Adress adress) {
-    adressRepository.save(adress);
+        adress.setApartment(adress.getApartment());
+        adressRepository.save(adress);
     }
 
     @Override
