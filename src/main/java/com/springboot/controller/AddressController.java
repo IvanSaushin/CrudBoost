276bdaead86891d8ff37c4320caa4bc4dbea45dc
mishe <mishelsim2@gmail.com>
2023-03-02 06:05:08 +0400

    МТ-051 Добавлено логгирование МТ-052 Имеются сложности с logback.xml, ожидаю вводных на созвоне

--- src/main/java/com/springboot/controller/AddressController.java
+++ src/main/java/com/springboot/controller/AddressController.java
@@ -2,6 +2,8 @@ package com.springboot.controller;
 
 import com.springboot.model.Address;
 import com.springboot.service.AddressService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,33 +13,41 @@ import java.util.List;
 @RestController
 @RequestMapping("/addresses/")
 public class AddressController {
+
+    private static final Logger log = LoggerFactory.getLogger(AddressController.class);
+
     @Autowired
     private AddressService addressService;
 
     @GetMapping("/getAll")
     public List<Address> findAll()  {
+        log.info("GET запрос на /addresses/getAll");
         return addressService.findAll();
     }
 
     @GetMapping("/getById/{id}")
     public Address findById(@PathVariable("id") Long id) {
+        log.info("GET запрос на /addresses/getById{id}");
         return addressService.findById(id);
     }
 
     @PostMapping("/save")
     public ResponseEntity<Address> create(@RequestBody Address address) {
         addressService.save(address);
+        log.info("POST запрос на /addresses/save");
         return ResponseEntity.ok().body(address);
     }
 
     @PutMapping("/update")
     public ResponseEntity<Address> update(@RequestBody Address address) {
+        log.info("PUT запрос на /addresses/update");
         addressService.update(address);
         return ResponseEntity.ok().body(address);
     }
 
     @DeleteMapping("/deleteById/{id}")
     public void delete(@PathVariable("id") Long id) {
+        log.info("DELETE запрос на /addresses/deleteById/{id}");
         addressService.deleteById(id);
     }
 }
--- src/main/java/com/springboot/controller/MainRestController.java
+++ src/main/java/com/springboot/controller/MainRestController.java
@@ -3,6 +3,8 @@ package com.springboot.controller;
 import com.springboot.model.User;
 import com.springboot.service.RoleService;
 import com.springboot.service.impl.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.List;
 @RequestMapping("/r-api/")
 public class MainRestController {
 
+    private static final Logger log = LoggerFactory.getLogger(MainRestController.class);
     @Autowired
     private UserServiceImpl userService;
     @Autowired
@@ -20,35 +23,41 @@ public class MainRestController {
 
     @GetMapping("/getAllUsers")
     public List<User> getAllUsers() {
+        log.info("GET запрос на /r-api/getAllUsers");
         return userService.getAllUsers();
     }
 
     @GetMapping("/getUser/{id}")
     public User getUser(@PathVariable("id") Long id) {
+        log.info("GET запрос на /r-api/getUser/{id}");
         return userService.getUserById(id);
     }
 
     @GetMapping("/search")
     public List<User> search (@RequestParam ("name") String searchName) {
         List<User> list = userService.findUsersBySearch(searchName);
+        log.info("GET запрос на /r-api/search");
         return list;
     }
 
     @PostMapping("/create")
     public ResponseEntity<User> create(@RequestBody User user) {
         userService.saveUser(user);
+        log.info("POST запрос на /r-api/create");
         return ResponseEntity.ok().body(user);
     }
 
     @PutMapping("/update")
     public ResponseEntity<User> update(@RequestBody User user) {
         userService.update(user);
+        log.info("PUT запрос на /r-api/update");
         return ResponseEntity.ok().body(user);
     }
 
     @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable("id") Long id) {
         userService.deleteById(id);
+        log.info("DELETE запрос на /r-api/delete/{id}");
     }
 
 }
